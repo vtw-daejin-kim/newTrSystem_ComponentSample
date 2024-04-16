6825e1d9a1622a69524f6c56d87a5f25b1c7d8f2
vtw-daejin-kim <daejin.kim@vtw.co.kr>
2024-04-16 17:17:35 +0900

    2024-04-16 컴포넌트 업데이트에 따른 샘플페이지 로그인 값 임시 세팅
    2024-04-16 컴포넌트 업데이트에 따른 샘플페이지 로그인 값 임시 세팅

--- front/src/components/unit/CustomEditTable.js
+++ front/src/components/unit/CustomEditTable.js
@@ -11,7 +11,8 @@ const CustomEditTable = ({ keyColumn, columns, values, tbNm, handleYnVal, ynVal,
 
     const [ cookies ] = useCookies(["userInfo", "userAuth"]);
     const [ cdValList, setCdValList ] = useState({});
-    const empId = cookies.userInfo.empId;
+    const empId = "75034125-f287-11ee-9b25-000c2956283f";
+    //const empId = cookies.userInfo.empId;
     const date = moment();
 
     useEffect(() => { getCdVal(); }, []);
